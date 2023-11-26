90de345393d76fc10c4a7b290e313d52b1bf24ee
dteplov <d.teplov@apeclix.com>
2023-11-26 23:39:00 +0400

    Version 2

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
     podName := os.Getenv("POD_NAME")
     nodeName := os.Getenv("NODE_NAME")
 
-    fmt.Fprintf(w, "<h1>ВКР Теплова Д.Ю</h1>")
+    fmt.Fprintf(w, "<h1>ВКР Теплова Д.Ю.</h1>")
+    fmt.Fprintf(w, "<h2>Версия 2</h1>")
     fmt.Fprintf(w, "<p>Имя контейнера: %s</p>", podName)
     fmt.Fprintf(w, "<p>Имя сервера: %s</p>", nodeName)
 }
